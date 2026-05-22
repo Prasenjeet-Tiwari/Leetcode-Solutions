0a72f0284fcb0eaa732ae660361e13c623531a62
Prasenjeet-Tiwari <142239861+Prasenjeet-Tiwari@users.noreply.github.com>
2026-05-22 20:15:26 +0530

    Time: 0 ms (100%), Space: 42 MB (64.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,19 @@
 class Solution {
 
     public int tabulation(int m, int n){
-        int dp[][]=new int[m][n];
+        int dp[][]=new int[m+1][n+1];
 
-        for(int i=0; i<m; i++){
-                dp[i][0]=1;            
-        }
-        for(int i=0; i<n; i++){
-                dp[0][i]=1;            
-        }
-
-        for(int row=1; row<m; row++){
-            for(int col=1; col<n; col++){
-                dp[row][col]= dp[row-1][col] + dp[row][col-1];
+        for(int row=1; row<m+1; row++){
+            for(int col=1; col<n+1; col++){
+                if(row==1 && col==1){
+                    dp[row][col]=1;
+                    continue;
+                }
+                dp[row][col]= dp[row-1][col] + dp[row][col-1] ;
             }
         }
 
-        return dp[m-1][n-1];
+        return dp[m][n];
     }
     public int uniquePaths(int m, int n) {
 
