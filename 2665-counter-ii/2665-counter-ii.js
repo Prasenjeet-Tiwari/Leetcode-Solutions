4716163cc8c5cb4900427e22fe75e36bf4464a09
Prasenjeet-Tiwari <142239861+Prasenjeet-Tiwari@users.noreply.github.com>
2026-06-15 19:40:05 +0530

    Time: 52 ms (44.33%), Space: 55.8 MB (89.27%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,36 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var counter=0;
+    return{
+        increment: function(){
+            init +=1;
+            counter += 1;
+            return init;
+        },
+        decrement: function(){
+            init -=1;
+            counter -= 1;
+            return init;
+
+        },
+        reset: function(){
+            
+            init= init-counter;
+            counter=0;
+
+            return init;
+        }
+
+    };
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
