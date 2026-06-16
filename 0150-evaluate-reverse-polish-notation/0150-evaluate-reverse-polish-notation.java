4493e84bfe3d11c93c9e56e210555a0a7663bf52
Prasenjeet-Tiwari <142239861+Prasenjeet-Tiwari@users.noreply.github.com>
2026-06-16 09:32:57 +0530

    Time: 6 ms (88.91%), Space: 45.4 MB (44.98%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,29 +1,25 @@
-import java.util.Stack;
-
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> st = new Stack<>();
-        
-        for (int i = 0; i < tokens.length; i++) {
-            String curr = tokens[i];
-            
-            // Check if the current token is an operator
-            if (curr.equals("+") || curr.equals("-") || curr.equals("*") || curr.equals("/")) {
-                // The second operand is popped first
-                int b = st.pop();
-                // The first operand is popped second
-                int a = st.pop();
-                
-                // Perform the operation and push the result
-                if (curr.equals("+")) st.push(a + b);
-                else if (curr.equals("-")) st.push(a - b);
-                else if (curr.equals("*")) st.push(a * b);
-                else if (curr.equals("/")) st.push(a / b);
+        Stack<Integer> stack = new Stack<>();
+
+        for (String c : tokens) {
+            if (c.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (c.equals("-")) {
+                int second = stack.pop();
+                int first = stack.pop();
+                stack.push(first - second);
+            } else if (c.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (c.equals("/")) {
+                int second = stack.pop();
+                int first = stack.pop();
+                stack.push(first / second);
             } else {
-                // If it's a number string, convert it to an integer and push it
-                st.push(Integer.parseInt(curr));
+                stack.push(Integer.parseInt(c));
             }
         }
-        return st.pop();
+
+        return stack.peek();        
     }
 }
