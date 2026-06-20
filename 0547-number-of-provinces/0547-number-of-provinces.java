cbe439d227b4cbc962750e4a35e093b7e52718ad
Prasenjeet-Tiwari <142239861+Prasenjeet-Tiwari@users.noreply.github.com>
2026-06-21 01:16:41 +0530

    Time: 1 ms (86.95%), Space: 47.1 MB (73.23%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,47 +1,29 @@
 class Solution {
-    public void dfs(int src, List<List<Integer>> graph, boolean vis[]){
-
-        vis[src]=true;
-        //System.out.println(src);
-
-        for(int child: graph.get(src)){
-          if(!vis[child]){
-            dfs(child, graph, vis);
-          }
-        }
-    }
     public int findCircleNum(int[][] isConnected) {
-        
-        List<List<Integer>> graph= new ArrayList<>();
-        int v=isConnected[0].length;    //no. of nodes or vertices
 
-        for(int i=0; i< v; i++){
-            graph.add(new ArrayList<>());
-        }
+        //dfsUtil func structure
 
-        // Array to keep track of visited nodes and add connected nodes of each vertex
-        for(int src=0; src < v; src++){
-            int sizeDest = isConnected[0].length;
-            for(int dest=0; dest<sizeDest; dest++){
-                if(src!=dest && isConnected[src][dest]==1){
-                    graph.get(src).add(dest);
-                }
+        int count =0;
+        int n = isConnected.length; //total no. of vertices
+        boolean[] visited = new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                count++;
+                dfs(i,isConnected,visited);
             }
         }
+        return count;
+    }
 
-    //basic dfs call with util func
-        boolean[] vis=new boolean[v];
-        int components=0;
+    private void dfs(int index,int[][] isConnected,boolean[] visited){
+        visited[index]=true;
 
-        for(int i=0; i<v; i++){
-            if(!vis[i]){    //whenever not found increment components
-                components++;
-                dfs(i,graph,vis);
+        //finding the neigbours
+        for(int nei=0;  nei<isConnected[index].length;  nei++){ //similar to for(int child : list.get(src))
+            if(isConnected[index][nei]==1){                        //imp. add only if is connected (condition)
+                //neighbour 
+                if(!visited[nei]) dfs(nei,isConnected,visited);
             }
-
         }
-
-        // Return the total number of provinces(disconnected vertices as a group)
-        return components;
     }
 }
