e3178ea7c00909b065a1952bc64796fca84640db
Prasenjeet-Tiwari <142239861+Prasenjeet-Tiwari@users.noreply.github.com>
2026-07-16 20:32:16 +0530

    Time: 3 ms (22.02%), Space: 48 MB (23.93%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n=nums.length;
-        boolean dp[]=new boolean[n];
-        dp[0]=true;
-        for(int i=0;i<n; i++){
-            int val=nums[i];
-            int limit=Math.min(n-1, i+val);
-            if(dp[i]==true){
-                for(int j=i+1; j<=limit; j++){
-                    dp[j]=true;
-                }
+        int curr=nums[0];
+        for(int i=1;i<n; i++){
+            if(curr==0){
+                return false;
             }
+            int val=nums[i];
+            curr=Math.max(curr-1, val);
         }
-        return dp[n-1];
+        return true;
     }
 }
