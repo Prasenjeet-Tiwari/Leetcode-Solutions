eb0de463768688bfab3646c92c9b88f96bf3b7fa
Prasenjeet-Tiwari <142239861+Prasenjeet-Tiwari@users.noreply.github.com>
2026-08-02 05:32:49 +0530

    Time: 76 ms (5.12%), Space: 101.4 MB (5.01%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,68 +1,77 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    //dsu
+    class DSU {
+        int[] parent;
+        int[] size;
 
-    
-    // class dsu{
-    //     int parent[];
-    //     int size[];
-
-    //     dsu(int n)}{
-    //         parent=new parent[n];
-    //         size=new parent[n];
-    //         for (int i = 0; i < n; i++) {
-    //             parent[i] = i;
-    //             size[i] = 1;
-    //         }
-    //     }
-
-
-    //     public int find(int i) {
-    //         if (parent[i] == i) {
-    //             return i;
-    //         }
-    //         return parent[i] = find(parent[i]); // Path compression
-    //     }
-
-    //     public void union(int a, int b) {
-    //         int rootA = find(a);
-    //         int rootB = find(b);
-    //         if (rootA != rootB) {
-    //             if (size[rootA] < size[rootB]) {
-    //                 parent[rootA] = rootB;
-    //                 size[rootB] += size[rootA];
-    //             } else {
-    //                 parent[rootB] = rootA;
-    //                 size[rootA] += size[rootB];
-    //             }
-    //         }
-    //     }
-    // }
-    public int longestConsecutive(int[] nums) {
-        if(nums.length==0  ){
-            return 0;
-        }
-        int longestStreak=1;
-
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
-        }
-
-        for (int num : set) {
-           
-            if (!set.contains(num - 1)) {
-                int currentNum = num;
-                int currentStreak = 1;
-
-                while (set.contains(currentNum + 1)) {
-                    currentNum++;
-                    currentStreak++;
-                }
-
-                longestStreak = Math.max(longestStreak, currentStreak);
+        DSU(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
             }
         }
 
-        return longestStreak;
+        public int find(int i) {
+            if (parent[i] == i) {
+                return i;
+            }
+            return parent[i] = find(parent[i]); // Path compression
+        }
+
+        public void union(int a, int b) {
+            int rootA = find(a);
+            int rootB = find(b);
+            if (rootA != rootB) {
+                if (size[rootA] < size[rootB]) {
+                    parent[rootA] = rootB;
+                    size[rootB] += size[rootA];
+                } else {
+                    parent[rootB] = rootA;
+                    size[rootA] += size[rootB];
+                }
+            }
+        }
+        
+        public int getMaxSize() {
+            int max = 0;
+            for (int s : size) {
+                max = Math.max(max, s);
+            }
+            return max;
+        }
+    }
+
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        DSU dsu = new DSU(nums.length);
+        // Map to store value -> index mapping to handle duplicates and track array positions
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                continue; // Skip duplicates to avoid self-unions or corrupted tracking
+            }
+            
+            map.put(nums[i], i);
+
+            // Connect with the smaller consecutive neighbor if it exists
+            if (map.containsKey(nums[i] - 1)) {
+                dsu.union(i, map.get(nums[i] - 1));
+            }
+            
+            // Connect with the larger consecutive neighbor if it exists
+            if (map.containsKey(nums[i] + 1)) {
+                dsu.union(i, map.get(nums[i] + 1));
+            }
+        }
+
+        return dsu.getMaxSize();
     }
 }
