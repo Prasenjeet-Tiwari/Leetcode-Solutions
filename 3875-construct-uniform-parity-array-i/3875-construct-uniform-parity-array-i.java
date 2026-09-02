748a8aec38de7aa678c8428c9d25f01c893d24b0
Prasenjeet-Tiwari <142239861+Prasenjeet-Tiwari@users.noreply.github.com>
2026-09-02 15:16:18 +0530

    Time: 0 ms (100%), Space: 45.3 MB (35.83%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // int no=0;
+        // int ne=0;
+        // int n=nums1.length;
+
+        // for(int x: num1){
+        //     if(x&1==1){
+        //         no++
+        //     }else{
+        //         ne++;
+        //     }
+        // }
+        // if(no==n ||ne==n){
+        //     return true;
+        // }
+        return true;
+        
+    }
+}
